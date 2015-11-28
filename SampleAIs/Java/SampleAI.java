9e8037df8335b0b089f0fdd73836c206e4ab0b96
kenkoooo <kenkou.n@gmail.com>
2015-11-28 12:41:45 +0900

    Update Java Sample AI

--- SampleAIs/Java/SampleAI.java
+++ SampleAIs/Java/SampleAI.java
@@ -1,131 +1,88 @@
-import java.util.Arrays;
+import java.io.PrintWriter;
 import java.util.Random;
 import java.util.Scanner;
 
+// sample random-walk AI
 public class SampleAI {
-    private final int BOARD_W = 18;
-    private final int FIELD_W = 6;
-    private final int BLOCK_W = BOARD_W / FIELD_W;
-    private final int PLAYER_N = 4;
+	private static Random random;
+	private static Scanner scanner;
+	private static PrintWriter writer;
 
-    private final char ATTACK = 'A';
-    private final char UP = 'U';
-    private final char RIGHT = 'R';
-    private final char DOWN = 'D';
-    private final char LEFT = 'L';
-    private final char NONE = 'N';
-    private final char[] ACTIONS = { ATTACK, UP, DOWN, RIGHT, LEFT };
+	private static int playerId;
+	private static int T;
+	private static int B[][];
+	private static int R[];
+	private static int C[];
+	private static String D[];
+	private static int S[];
+	private static String EOD;
 
-    private int id = 0, turn = 0;
-    private int[][] board = new int[FIELD_W][FIELD_W];
-    private int[] lifes = new int[PLAYER_N];
-    private int[] row = new int[PLAYER_N];
-    private int[] column = new int[PLAYER_N];
-    private char dir[] = new char[PLAYER_N];
-    private int[] pausing = new int[PLAYER_N];
+	private static int myLife;
+	private static int enemyLives;
+	private static String commands = "URDLAN";
 
-    private char action() {
-	for (int i = 0; i < PLAYER_N; i++) {
-	    if (i == id || board[i][0] < 0 || board[i][1] < 0)
-		continue;
-	    if (board[i][0] == board[id][0]) {
-		if (board[i][1] < board[id][1] && dir[id] == LEFT) {
-		    return ATTACK;
-		} else if (board[i][1] > board[id][1] && dir[id] == RIGHT) {
-		    return ATTACK;
+	public static void main(String[] args) {
+		B = new int[6][6];
+		R = new int[4];
+		C = new int[4];
+		D = new String[4];
+		S = new int[4];
+
+		if (args.length >= 2) {
+			if (args[0].equals("-s")) {
+				random = new Random(Long.parseLong(args[1]));
+			}
+		} else {
+			random = new Random();
 		}
-	    } else if (board[i][1] == board[id][1]) {
-		if (board[i][0] < board[id][0] && dir[id] == UP) {
-		    return ATTACK;
-		} else if (board[i][0] > board[id][0] && dir[id] == DOWN) {
-		    return ATTACK;
+		scanner = new Scanner(System.in);
+		writer = new PrintWriter(System.out, true);
+		writer.println("READY");
+		writer.flush();
+
+		while (true) {
+			readData();
+
+			enemyLives = 0;
+			for (int i = 0; i < 4; i++) {
+				if (i != playerId && !isDead(i)) {
+					enemyLives++;
+				}
+			}
+			if (isDead(playerId) || enemyLives == 0) {
+				break;
+			}
+
+			writeCommand();
 		}
-	    }
+
+		scanner.close();
+		writer.close();
 	}
 
-	for (int i = 0; i < PLAYER_N; i++) {
-	    if (i == id || board[i][0] < 0 || board[i][1] < 0)
-		continue;
-	    if (board[i][0] == board[id][0]) {
-		if (board[i][1] < board[id][1]) {
-		    return LEFT;
-		} else if (board[i][1] > board[id][1]) {
-		    return RIGHT;
+	private static boolean isDead(int id) {
+		return R[id] == -1 && C[id] == -1;
+	}
+
+	private static void readData() {
+		playerId = scanner.nextInt();
+		T = scanner.nextInt();
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 6; j++) {
+				B[i][j] = scanner.nextInt();
+			}
 		}
-	    } else if (board[i][1] == board[id][1]) {
-		if (board[i][0] < board[id][0]) {
-		    return UP;
-		} else if (board[i][0] > board[id][0]) {
-		    return DOWN;
+		for (int i = 0; i < 4; i++) {
+			R[i] = scanner.nextInt();
+			C[i] = scanner.nextInt();
+			D[i] = scanner.next();
+			S[i] = scanner.nextInt();
 		}
-	    }
+		EOD = scanner.next();
 	}
 
-	int[] blockDist = new int[PLAYER_N];
-	for (int i = 0; i < PLAYER_N; i++) {
-	    if (i == id || board[i][0] < 0 || board[i][1] < 0)
-		continue;
-	    blockDist[i] = Math.min(Math.abs(board[id][0] - board[i][0]),
-		    Math.abs(board[id][1] - board[i][1]));
+	private static void writeCommand() {
+		writer.println(commands.charAt(random.nextInt(commands.length())));
+		writer.flush();
 	}
-	int near = id;
-	for (int i = 0; i < PLAYER_N; i++) {
-	    if (i == id || board[i][0] < 0 || board[i][1] < 0)
-		continue;
-	    if (near == id || blockDist[near] > blockDist[i]) {
-		near = i;
-	    }
-	}
-	if (near == id) {
-	    return ATTACK;
-	}
-
-	if (Math.abs(board[near][0] - board[id][0]) > Math.abs(board[near][1]
-		- board[id][1])) {
-	    if (board[id][1] > board[near][1]) {
-		return LEFT;
-	    } else {
-		return RIGHT;
-	    }
-	} else {
-	    if (board[id][0] > board[near][0]) {
-		return DOWN;
-	    } else {
-		return UP;
-	    }
-	}
-    }
-
-    public void run() {
-	System.out.println("READY");
-	Scanner scanner = new Scanner(System.in);
-
-	while (true) {
-	    id = scanner.nextInt();
-	    turn = scanner.nextInt();
-	    for (int i = 0; i < lifes.length; i++) {
-		lifes[i] = scanner.nextInt();
-	    }
-
-	    for (int i = 0; i < board.length; i++) {
-		for (int j = 0; j < board[i].length; j++) {
-		    board[i][j] = scanner.nextInt();
-		}
-	    }
-	    for (int i = 0; i < PLAYER_N; i++) {
-		row[i] = scanner.nextInt();
-		column[i] = scanner.nextInt();
-		dir[i] = scanner.next().toCharArray()[0];
-		pausing[i] = scanner.nextInt();
-	    }
-	    scanner.next();// EOD
-
-	    System.out.println(action());
-	}
-    }
-
-    public static void main(String[] args) {
-	new SampleAI().run();
-    }
-
 }
